75fce6e7ccdca29be6a7747971dd8e61237761ae
tobi <noggnzzz@gmail.com>
2024-02-19 14:21:27 -0500

    geodb updated

--- js/db/geodb.js
+++ js/db/geodb.js
@@ -50,8 +50,8 @@ export const geodb = [
     fuente: 'INEI 2017',
     keyword: ['peru', 'red', 'rios', 'agua', 'hidrografica', 'hidrografia', 'lagos', 'lagunas', 'cuenca'],
     categoria: 1,
-    img: 'https://raw.githubusercontent.com/1nfinit0/geospatial-repo/master/db/category/political-geography/peru-red-hidrografica/peru-red-hidrografica.jpg',
-    data: 'https://raw.githubusercontent.com/1nfinit0/geospatial-repo/master/db/category/political-geography/peru-red-hidrografica/peru-red-hidrografica.js',
-    descarga: {shp: 'https://github.com/1nfinit0/geospatial-repo/blob/master/db/category/political-geography/peru-red-hidrografica/peru-red-hidrografica-shp.zip'}
+    img: 'https://raw.githubusercontent.com/1nfinit0/geospatial-repo/master/db/category/physical-geography/peru-red-hidrografica/peru-red-hidrografica.jpg',
+    data: 'https://raw.githubusercontent.com/1nfinit0/geospatial-repo/master/db/category/physical-geography/peru-red-hidrografica/peru-red-hidrografica.js',
+    descarga: {shp: 'https://github.com/1nfinit0/geospatial-repo/blob/master/db/category/physical-geography/peru-red-hidrografica/peru-red-hidrografica-shp.zip'}
   },
 ]
